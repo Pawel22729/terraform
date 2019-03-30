6b7022d550fd9e47009e950fd6a042a3a408bd6d
Pablo <pawel22729@gmail.com>
2019-03-30 19:46:07 +0100

    play with ecs

--- ecs/asg.tf
+++ ecs/asg.tf
@@ -1,18 +1,17 @@
-resource "aws_launch_template" "test" {
-  name_prefix   = "test"
-  image_id      = "ami-0380c676fcff67fd5"
-  instance_type = "t2.micro"
-}
-
 resource "aws_launch_configuration" "test" {
   name          = "test"
-  image_id      = "ami-0380c676fcff67fd5"
-  instance_type = "t2.micro"
+  image_id      = "ami-0376206bb575c76dd"
+  instance_type = "t3.small"
+  iam_instance_profile = "ecsInstanceRole"
+  user_data = <<EOF
+      #!/bin/bash
+      echo ECS_CLUSTER=test >> /etc/ecs/ecs.config
+      EOF
 }
 resource "aws_autoscaling_group" "test" {
   name                 = "terraform-asg-example"
   launch_configuration = "${aws_launch_configuration.test.name}"
-  availability_zones = ["eu-west-2a"]
+  availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   min_size             = 1
   max_size             = 1
 }
