fceaf209d0b6f87a51cf23664a01fee8e2b4a8a8
Pablo <pawel22729@gmail.com>
2019-06-05 19:43:36 +0200

    Now you know why /location/ wasn't working yeh?

---
+++ codebuild/docebuild.tf
@@ -0,0 +1,93 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "example-mala-madzia-duzy-brzuszek"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "example" {
+  name = "example"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "example" {
+  role = "${aws_iam_role.example.name}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.example.arn}",
+        "${aws_s3_bucket.example.arn}/*"
+      ]
+    },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "*"
+        ],
+        "Resource": [
+            "*"
+        ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_codebuild_project" "example" {
+  name          = "test-project"
+  description   = "test_codebuild_project"
+  build_timeout = "5"
+  service_role  = "${aws_iam_role.example.arn}"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type     = "NO_CACHE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:2.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+  }
+
+  source {
+    type            = "CODECOMMIT"
+    location        = "https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/test-test-test"
+  }
+}
